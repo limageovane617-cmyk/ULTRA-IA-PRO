7b952829adc403282794146707910ae61edc7e8c
limageovane617-cmyk <limageovane617@gmail.com>
2026-08-29 23:00:25 -0300

    Update button dimensions in app.py

--- app.py
+++ app.py
@@ -203,11 +203,11 @@ CSS = f"""
 
         padding: 0 !important;
 
-        min-width: 1px !important;
+        min-width: 38px !important;
 
-        width: 1px !important;
+        width: 38px !important;
 
-        height: 1px !important;
+        height: 38px !important;
 
         border-radius: 100% !important;
 
