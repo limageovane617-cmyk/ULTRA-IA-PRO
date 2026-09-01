240785482b903fcc86191cd630d25d3a32a4c308
limageovane617-cmyk <limageovane617@gmail.com>
2026-09-01 16:56:18 -0300

    Enable immersive sticky mode in MainActivity
    Added immersive sticky mode to the system UI for fullscreen experience.

--- android/app/src/main/java/com/geovani/alexiaultra/MainActivity.java
+++ android/app/src/main/java/com/geovani/alexiaultra/MainActivity.java
@@ -3,6 +3,7 @@ package com.geovani.alexiaultra;
 import android.app.Activity;
 import android.os.Bundle;
 import android.graphics.Color;
+import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
@@ -25,6 +26,15 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
 
         try {
             criarTela();
@@ -161,4 +171,3 @@ public class MainActivity extends Activity {
         super.onDestroy();
     }
 }
- 
