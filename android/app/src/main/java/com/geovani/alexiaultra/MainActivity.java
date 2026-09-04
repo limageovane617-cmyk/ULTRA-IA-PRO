a128b8468cace01eae590f99505c2cd51a95ac1f
limageovane617-cmyk <limageovane617@gmail.com>
2026-09-04 09:24:31 -0300

    Remove extra closing brace in MainActivity.java

--- android/app/src/main/java/com/geovani/alexiaultra/MainActivity.java
+++ android/app/src/main/java/com/geovani/alexiaultra/MainActivity.java
@@ -1288,7 +1288,6 @@ public class MainActivity extends Activity {
             }
         });
     }
-}
      
     // ============================================================
     // LER RESPOSTA DO SERVIDOR
